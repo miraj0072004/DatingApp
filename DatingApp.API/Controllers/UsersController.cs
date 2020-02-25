d35e19b072ef7d6edea1c3a198e51de451d27f79
miraj0072004@gmail.com <miraj0072004@gmail.com>
2020-02-25 21:13:15 +1100

    Extending The Api

---
+++ DatingApp.API/Controllers/UsersController.cs
@@ -0,0 +1,48 @@
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using AutoMapper;
+using DatingApp.API.Data;
+using DatingApp.API.Dtos;
+using DatingApp.API.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DatingApp.API.Controllers
+{
+    [Authorize]
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UsersController : ControllerBase
+    {
+        private readonly IDatingRepository repo;
+        private readonly IMapper mapper;
+
+        public UsersController(IDatingRepository repo, IMapper mapper)
+        {
+            this.mapper = mapper;
+            this.repo = repo;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetUsers()
+        {
+            var users = await repo.GetUsers();
+            var usersToReturn = mapper.Map<IEnumerable<UserForListDto>>(users);
+            return Ok(usersToReturn);
+        }
+
+        private object IEnumerable<T>(IEnumerable<User> users)
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetUser(int id)
+        {
+            var user = await repo.GetUser(id);
+            var userToReturn = mapper.Map<UserForDetailedDto>(user);
+            return Ok(userToReturn);
+        }
+    }
+}
